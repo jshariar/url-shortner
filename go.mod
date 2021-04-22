0dd1adfda3ade0d08ce78b77154d1cded3f54c19
Shariar Joy <jshariar@live.com>
2021-04-22 01:50:51 -0400

    commit on master

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jshariar/url-shortner
 
 go 1.16
+
+require github.com/joho/godotenv v1.3.0
--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
-)
-
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error Loading env file")
-	}
 
-	fmt.Println("Output:", os.Getenv("DB_CONNECTION_STRING"))
 }
