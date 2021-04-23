eaf1dd138c9ac70301f6ee82e3ea7c096d00bf18
Shariar Joy <jshariar@live.com>
2021-04-22 23:00:08 -0400

    routing started

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"path"
+
+	"github.com/gorilla/mux"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	p := path.Dir("index.html")
+
+	w.Header().Set("Content-type", "text/html")
+	http.ServeFile(w, r, p)
+}
+
+func handleRequests() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", homePage).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
 func main() {
+
+	handleRequests()
 
 }
